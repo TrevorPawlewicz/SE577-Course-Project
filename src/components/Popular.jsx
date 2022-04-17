25477bfe6dbe265435715060f45bafd00148048a
Trevor Pawlewicz <trevor_pawlewicz@comcast.com>
2022-04-17 15:37:56 -0700

    react project added

---
+++ src/components/Popular.jsx
@@ -0,0 +1,7 @@
+function Popular(){
+  return (
+    <div>Popular</div>
+  )
+}
+
+export default Popular;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Core from "../components/Core";
+import Popular from "../components/Popular";
+//-------------------------------------------
+
+function Home(){
+  return (
+    <div>
+      <Core />
+      <Popular />
+    </div>
+  );
+}
+
+export default Home;
